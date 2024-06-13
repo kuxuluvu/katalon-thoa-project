c425149c780f36d60299d3eab3cf3f7a0b097bbc
kuxuluvu <vuduc762@gmail.com>
2024-06-13 11:23:55 +0700

    add checkpoints

---
+++ Test Suites/Webs/TS03 - Checkpoint.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS03 - Checkpoint</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f37912e5-d330-4a48-bf92-59e3b7279ec0</testSuiteGuid>
+   <testCaseLink>
+      <guid>c62e608f-d3b9-4c6b-89cf-af8f04f3df4f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Webs/TC03 - Checkpoint</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
