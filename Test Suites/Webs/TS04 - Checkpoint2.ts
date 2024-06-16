849ad1c870497f2d91b68d6d0beadad48fafd6a5
kuxuluvu <vuduc762@gmail.com>
2024-06-16 21:22:14 +0700

    add checkpoint

---
+++ Test Suites/Webs/TS04 - Checkpoint2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS04 - Checkpoint2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>280ba5be-850b-4fd2-982c-d28758019625</testSuiteGuid>
+   <testCaseLink>
+      <guid>8a60a85b-0919-4eb0-8636-f85b697e18d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Webs/TC04 - Checkpoint 2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
